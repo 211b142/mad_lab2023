af4e46072d107a18f113f2337427662ed0c36fa8
ratnesh tiwari <ratnesh.tiwari@JIET.CO.IN>
2023-03-22 15:35:52 +0530

    animation

--- MainActivity.java
+++ MainActivity.java
@@ -2,29 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-    boolean isdora = true;
-
-    public void change(View view) {
-        ImageView iv = findViewById(R.id.imageView);
-        if (isdora) {
-            iv.setImageResource(R.drawable.img2);
-            isdora = false;
-        } else {
-            iv.setImageResource(R.drawable.d);
-            isdora = true;
+public class MainActivity extends AppCompatActivity{
+    boolean ismicrophone=true;
+    public void change(View view){
+        ImageView imageView6=findViewById(R.id.imageView6);
+        ImageView imageView2=findViewById(R.id.imageView2);
+        if(ismicrophone) {
+            imageView6.animate().alpha(1).setDuration(2000);
+            imageView2.animate().alpha(0).setDuration(2000);
+            ismicrophone = false;
+        }else{
+            imageView6.animate().alpha(0).setDuration(2000);
+            imageView2.animate().alpha(1).setDuration(2000);
+            ismicrophone=true;
         }
-    }    
-        
-   
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView=findViewById(R.id.imageView2);
+        imageView.animate().setDuration(2000).rotation(360);
     }
 }
